b7fbe2f756f6b78490fe72cb4c774e6ed403aba9
lxj <1056587277@qq.com>
2019-10-07 16:28:01 +0800

    init repo

---
+++ src/main/java/life/lxj/commity/CommityApplication.java
@@ -0,0 +1,13 @@
+package life.lxj.commity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/life/lxj/commity/controller/HelloController.java
@@ -0,0 +1,20 @@
+package life.lxj.commity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by asus on 2019/10/7.
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
